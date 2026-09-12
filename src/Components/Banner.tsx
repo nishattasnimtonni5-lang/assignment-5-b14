4c53810e64d65e74036ac5f033a948fa282d6306
nishattasnimtonni5-lang <nishattasnimtonni5@gmail.com>
2026-09-12 17:35:04 +0600

    style:imrove banner styling

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -2,25 +2,30 @@ import React from 'react';
 import banner from "../assets/banner-stack.png"
 const Banner = () => {
     return (<div>
-        <div className="flex items-center">
-            <div className="pl-7 ">
+        <div className="flex items-center pt-10">
+        <div  >
+            <div className="pl-7 pb-10  ">
                 <h1 className="text-6xl font-bold ">Build Your Ideal <span className="bg-gradient-to-r from-orange-600 via-pink-500 to-violet-500 bg-clip-text text-transparent"> Develpment Stack </span></h1>
-          <p className="text-gray-500 pt-4 text-sm pr-50 ">Explore fronted,backend,database,and tooling options,    
+          <p className="text-gray-500 pt-4 text-sm pr-50px ">Explore fronted,backend,database,and tooling options,    
            compare them side by side,and put together the stack that fits your next project. </p>
-            </div>
-            <img src={banner} alt="Banner" className="w-100 h-105"/>
-            </div>
-            <div className="flex gap-4 pl-7 p ">
+           </div> 
+            <div className="flex gap-4 pl-7   ">
             <button className="bg-gradient-to-r from-orange-400 to-pink-500 text-white font-bold hover:bg-none hover:bg-amber-50 hover:text-pink-500  rounded-md py-3 px-3 ">Explore Technologies</button>
-            <button className="border rounded-md py-3 px-13 border border-gray-400 text-gray-500 font-bold hover:bg-gray-500 hover:text-white">Learn More</button>
+            <button className="rounded-md py-3 px-13 border border-gray-400 text-gray-500 font-bold hover:bg-gray-500 hover:text-white">Learn More</button>
         
-        </div>
-        <div className="pl-5 pt-15 pb-5">
-            <h1 className="text-3xl font-bold">Explore the <span className="text-pink-500">Technologies</span></h1>
-       <p className="text-gray-500 pt-2 text-sm">Pick one technology per category to build your ideal stack</p>
+          </div>
+          </div>
+        
+            <img src={banner} alt="Banner" className="w-100"/>
+            </div>
+              
+           
+             <div className="pl-5 pt-15 pb-5">
+              <h1 className="text-3xl font-bold">Explore the <span className="text-pink-500">Technologies</span></h1>
+             <p className="text-gray-500 pt-2 text-sm">Pick one technology per category to build your ideal stack</p>
         </div>
     
-      </div>
+     </div>
     );
 };
 
